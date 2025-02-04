956f38ebaf84275be20dc6877a8c727eda1d8dd1
chris <Mileschris466@gmail.com>
2025-02-04 23:59:25 +0300

    changed password

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'Awilo9701',
   database: process.env.DB_NAME || 'mydb'
 });
 
