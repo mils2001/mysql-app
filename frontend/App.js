2a952240c2c295313920290006029c1003f5633c
chris <Mileschris466@gmail.com>
2025-02-14 15:42:57 +0300

    app.js

---
+++ frontend/App.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('/users')
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error('Error fetching users:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Users List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.email})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
