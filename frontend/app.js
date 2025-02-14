09327af0549e9f4df584b6a29c524e6216b26a64
chris <Mileschris466@gmail.com>
2025-02-14 16:57:22 +0300

    updates on html and css

---
+++ frontend/app.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('/users')
+      .then(response => setUsers(response.data))
+      .catch(error => console.error('Error fetching users:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name} ({user.email})</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';  // Optional if you want to style your app
+
+const App = () => {
+  return <h1>Hello from React!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
