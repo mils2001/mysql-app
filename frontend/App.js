ddac9b7f8f19c3079e270bbb416db81e335c132c
chris <Mileschris466@gmail.com>
2025-02-14 15:59:56 +0300

    update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,27 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const App = () => {
+function App() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch('/users')
-      .then((response) => response.json())
-      .then((data) => setUsers(data))
-      .catch((error) => console.error('Error fetching users:', error));
+    axios.get('/users')
+      .then(response => setUsers(response.data))
+      .catch(error => console.error('Error fetching users:', error));
   }, []);
 
   return (
     <div>
-      <h1>Users List</h1>
+      <h1>User List</h1>
       <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.name} ({user.email})
-          </li>
+        {users.map(user => (
+          <li key={user.id}>{user.name} ({user.email})</li>
         ))}
       </ul>
     </div>
   );
-};
+}
 
 export default App;
+
